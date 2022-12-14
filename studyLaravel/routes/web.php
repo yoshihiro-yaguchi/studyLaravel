e587deb967c03bc5461d142262032b6d8dbee6cf
yoshihiro-yaguchi <yoshihiro19990307@outlook.com>
2022-12-14 23:36:25 +0900

    2-1 リスト2-4

--- studyLaravel/routes/web.php
+++ studyLaravel/routes/web.php
@@ -20,3 +20,17 @@ Route::get('/', function () {
 Route::get('/hello', function() {
     return '<html><body><h1>hello</h1><p>This is sample page.</p></body></html>';
 });
+
+$html = <<<EOF
+<html>
+    <body>
+        <h1>Hello</h1>
+        <p>This is sample page.</p>
+        <p>これはサンプルで作ったページです。</p>
+    </body>
+</html>
+EOF;
+
+Route::get('/hello/hereDoc', function () use ($html) {
+    return $html;
+});
